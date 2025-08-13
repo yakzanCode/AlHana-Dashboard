b4252fccbc76c2133a09fe43646f09c8159b0aa3
Yakzan Code <yakzancode@outlook.com>
2025-08-13 15:59:18 +0200

    first ever commit

---
+++ schemaTypes/dish.ts
@@ -0,0 +1,56 @@
+import {defineType, defineField} from 'sanity'
+
+export const dish = defineType({
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: Rule => Rule.required()
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: Rule => Rule.required()
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: Rule => Rule.required().min(0)
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          options: { hotspot: true }
+        }
+    ]
+    }),
+    defineField({
+      name: 'mainDish',
+      title: 'Main Dish',
+      type: 'boolean',
+      description: 'Set true if this is the main dish'
+    }),
+    defineField({
+      name: 'isAvailable',
+      title: 'Is Available',
+      type: 'boolean',
+      description: 'Set false if the dish is currently unavailable'
+    })
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'images.[0]'
+    }
+  }
+})
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import { dish } from "./dish";
+
+export const schemaTypes = [dish]
+// sanity documents query "*[_type == \"dish\"]"
