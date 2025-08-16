0fde28903ff8d2aa715adbba53827f723ddbba97
Yakzan Code <yakzancode@outlook.com>
2025-08-17 01:51:32 +0200

    fewfd

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,4 +3,4 @@ import { feedBack } from "./feedBack";
 import { teamMember } from "./teamMember";
 import { user } from "./user";
 
-export const schemaTypes = [dish, user, feedBack]
+export const schemaTypes = [dish, user, feedBack, teamMember]
--- schemaTypes/user.ts
+++ schemaTypes/user.ts
@@ -24,5 +24,13 @@ export const user = defineType({
       type: 'image',
       options: { hotspot: true },
     }),
+    
+    defineField({
+      name: 'feedbacks',
+      title: 'Feedbacks',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'feedBack' }] }],
+      description: 'Feedback written by this yuser'
+    }),
   ],
 })
